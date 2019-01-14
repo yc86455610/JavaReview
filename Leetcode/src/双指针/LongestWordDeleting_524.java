4490bfa5944533ab58758a6e2aaad5fb03616530
yc2698676480 <18911423879@163.com>
2019-01-14 22:07:07 +0800

    update sorting

--- Leetcode/src/双指针/LongestWordDeleting_524.java
+++ Leetcode/src/双指针/LongestWordDeleting_524.java
@@ -25,21 +25,21 @@ public class LongestWordDeleting_524 {
 
 }
 
-class Solution {
-    public String findLongestWord(String s, List<String> d) {
-        String longestWord = "";
-        for (String target: d){
-            int l1 = longestWord.length(), l2 = target.length();
-
-
-        }
-
-
-        return longestWord;
-    }
-
-    private boolean isValid(String s, String target){
-        int i = 0, j = 0 ;
-
-    }
-}
+//class Solution {
+//    public String findLongestWord(String s, List<String> d) {
+//        String longestWord = "";
+//        for (String target: d){
+//            int l1 = longestWord.length(), l2 = target.length();
+//
+//
+//        }
+//
+//
+//        return longestWord;
+//    }
+//
+//    private boolean isValid(String s, String target){
+//        int i = 0, j = 0 ;
+//
+//    }
+//}
