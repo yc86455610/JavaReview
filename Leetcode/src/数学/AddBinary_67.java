28c7e79f826b227ab3b169e18392392fbca1dcf3
yc86455610 <2698676480@qq.com>
2019-01-15 16:48:06 +0800

    delete addBinary.java

--- Leetcode/src/数学/AddBinary_67.java
+++ Leetcode/src/数学/AddBinary_67.java
@@ -17,34 +17,4 @@ import java.sql.SQLOutput;
  * 输出: "10101"
  */
 
-public class AddBinary_67 {
 
-    public static void main(String[] args) {
-        Solution_67 solution_67 = new Solution_67();
-        System.out.println(solution_67.addBinary("11", "1"));
-    }
-
-
-}
-
-
-class Solution_67 {
-    public String addBinary(String a, String b) {
-
-        //carry表示当前位
-        int i = a.length() - 1, j = b.length() - 1, carry = 0;
-        StringBuilder str = new StringBuilder();
-        while (carry == 1 || i >= 0 || j >= 0) {
-            if (i >= 0 && a.charAt(i--) == '1') {
-                carry++;
-            }
-            if (j >= 0 && b.charAt(j--) == '1') {
-                carry++;
-            }
-            str.append(carry % 2);   //1+1逢二进一
-            carry /= 2;
-        }
-        return str.reverse().toString();
-
-    }
-}
