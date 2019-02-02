2ed8c58d6ba387a34af15458a2ae13c4395e46bb
yc86455610 <2698676480@qq.com>
2019-02-02 13:33:44 +0800

    update permutation

--- Offer/src/38_字符串的排列/Test38.java
+++ Offer/src/38_字符串的排列/Test38.java
@@ -1,51 +1,51 @@
+import javax.security.auth.callback.CallbackHandler;
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.concurrent.RecursiveTask;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.IdentityHashMap;
 
 /**
  * 输入一个字符串，按字典序打印出该字符串中字符的所有排列。
  * 例如输入字符串 abc，则打印出由字符 a, b, c 所能排列出来的所有字符串 abc, acb, bac, bca, cab 和 cba。
+ * <p>
+ * 1、全排列
+ * 2、所有字母的组合
  */
 
 class Test38 {
     public static void main(String[] args) {
         Solution38 solution38 = new Solution38();
-        String str = new String("abc");
-        System.out.println(solution38.Permutation(str));
-
+        System.out.println(solution38.Permutation("abc"));
     }
 }
 
 class Solution38 {
-    ArrayList<String> ret = new ArrayList<>();
-
     public ArrayList<String> Permutation(String str) {
-        if (str.length() == 0)
-            return ret;
-        char[] chars = str.toCharArray();
-        Arrays.sort(chars);
-        backtracking(chars, new boolean[chars.length], new StringBuilder());
-
-        return ret;
+        ArrayList<String> res = new ArrayList<>();
+        if (str.length() < 1 || str == null)
+            return res;
+        Permutation(str.toCharArray(), 0, res);
+        Collections.sort(res);
+        return res;
     }
 
-    //回溯方法
-    private void backtracking(char[] chars, boolean[] hasUsed, StringBuilder s) {
-        if (s.length() == chars.length) {
-            ret.add(s.toString());
-            return;
+    private void Permutation(char[] chs, int index, ArrayList<String> list) {
+        if (index == chs.length - 1)
+            list.add(new String(chs));
+        for (int i = index; i < chs.length; i++) {
+            if (i == index || chs[i] != chs[index]) {
+                swap(chs, i, index);
+                Permutation(chs, index + 1, list);
+                swap(chs, i, index);
+            }
         }
-        for (int i = 0; i < chars.length; i++) {
-            if (hasUsed[i])
-                continue;
-            //保证不重复
-            if (i != 0 && chars[i] == chars[i - 1] && !hasUsed[i - 1])
-                continue;
-            hasUsed[i] = true;
-            s.append(chars[i]);
-            backtracking(chars, hasUsed, s);
-            s.deleteCharAt(s.length() - 1);
-            hasUsed[i] = false;
-        }
+    }
+
+    private void swap(char[] chs, int i, int j) {
+        char temp = chs[i];
+        chs[i] = chs[j];
+        chs[j] = temp;
     }
 }
