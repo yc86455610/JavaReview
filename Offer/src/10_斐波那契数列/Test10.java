e7950983ec8bdc785ca146a6aa586e4315180aa0
yc86455610 <2698676480@qq.com>
2019-02-03 16:28:58 +0800

    some update

--- Offer/src/10_斐波那契数列/Test10.java
+++ Offer/src/10_斐波那契数列/Test10.java
@@ -1,21 +1,30 @@
 import jdk.swing.interop.SwingInterOpUtils;
 
+import java.sql.ResultSet;
+
 /**
  * 10.1 斐波那契数列, 求斐波那契数列的第 n 项，n <= 39。
  * 递归的问题： 会重复计算一些子问题，动态规划把子问题的解缓存起来，避免求解子问题
  * <p>
  * 10.2 青蛙跳台阶
+ * 一只青蛙一次可以跳上 1 级台阶，也可以跳上 2 级。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
  * <p>
  * 10.3 矩形覆盖
+ * 2*1 的小矩形横着或者竖着去覆盖更大的矩形,请问用 n 个 2*1 的小矩形无重叠地覆盖一个 2*n 的大矩形，
+ * 总共有多少种方法？
  * <p>
- * 10.4 变态跳台阶
+ * 10.4 变态跳台阶一只青蛙一次可以跳上 1 级台阶，也可以跳上 2 级... 它也可以跳上 n 级。
+ * 求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
  */
 
 class Test10 {
     public static void main(String[] args) {
         Solution10 solution10 = new Solution10();
 //        System.out.println(solution10.Fibonacci_1(4));
-        System.out.println(solution10.Fibonacci(4));
+//        System.out.println(solution10.Fibonacci(4));
+//        System.out.println(solution10.JumpFloor(4));
+        System.out.println(solution10.RectCover(4));
+
     }
 }
 
@@ -48,4 +57,35 @@ class Solution10 {
         }
         return fib;
     }
+
+    public int JumpFloor(int n) {
+        if (n <= 2)
+            return n;
+        int pre2 = 1, pre1 = 2;
+        int result = 1;
+        for (int i = 3; i <= n; i++) {
+            result = pre1 + pre2;
+            pre2 = pre1;
+            pre1 = result;
+        }
+        return result;
+    }
+
+    public int RectCover(int n) {
+        if (n <= 2)
+            return n;
+        int pre2 = 1, pre1 = 2;
+        int result = 0;
+        for (int i = 3; i <= n; i++) {
+            result =pre1 + pre2;
+            pre2 = pre1;
+            pre1 = result;
+        }
+        return result;
+    }
+
+    public int JumpFloorII(int target){
+
+    }
+
 }
